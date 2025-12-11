8a60e63f67edf31963ac5205559ff2b259276fc1
adesoky1982a-dotcom <adesoky1982a@gmail.com>
2025-12-11 17:01:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('ok');
+// Smooth scroll for navbar links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
